8cbd5085b274300af12d69ff6742cfba8ae1c835
Javier Tobar <javier.tobarvillagra@gmail.com>
2026-02-01 16:23:56 -0300

    Eliminar carpeta Cara-rondando del portafolio

--- Cara-rondando/ejecutar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start python cara_rondando.py
-REM el start es para que abra otra CMD :P
-REM .\ejecutar.bat
-REM puedes crear un archivo directo de "ejecutar.bat" para no tener que abrir la CMD y escribir el comando :D
